f6f3911daaca0830891b9a2e9c8df57470297091
Deya Genao <deyagenao@gmail.com>
2019-09-11 23:47:23 -0400

    added details to event display and map

--- assets/javascript/event-search-box.js
+++ assets/javascript/event-search-box.js
@@ -65,11 +65,11 @@ $("#search-events").on("click", function(event){
         for (var i=0; i < response._embedded.events.length; i++){
             var eventImageSrc = response._embedded.events[i].images[8].url;
             var eventName = response._embedded.events[i].name;
+            var eventLink = response._embedded.events[i].url;
             var eventVenue = response._embedded.events[i]._embedded.venues[0].name;
             var eventAddress = response._embedded.events[i]._embedded.venues[0].address.line1+ " " + response._embedded.events[i]._embedded.venues[0].city.name+ ", " + response._embedded.events[i]._embedded.venues[0].state.name;
             var eventDate = moment(response._embedded.events[i].dates.start.localDate, "YYYY-MM-DD").format("MMM Do YYYY");
             //eventPrice
-            //eventURL --> to buy tickets
             //eventStartTime
             
             //testing
@@ -79,19 +79,24 @@ $("#search-events").on("click", function(event){
             var newEventElement = $("<div>").attr("id", "event-"+eventCounter);
             var eventImage = $("<img>").attr("src", eventImageSrc).css("height", "100px").addClass("float-right ml-2");
             newEventElement.append(eventImage);
-            var eventNameElement = $("<h4>").text(eventName);
+            var eventNameElement = $("<a>").html("<h4>" + eventName + "</h4>");
+            eventNameElement.attr("href", eventLink);
+            eventNameElement.attr("target", "_blank");
             newEventElement.append(eventNameElement);
-            var eventVenueElement = $("<h5>").text( eventVenue);
+            var eventVenueElement = $("<h5>").text(eventVenue);
             newEventElement.append(eventVenueElement);
             var eventAddressElement = $("<p>").text("Address: " + eventAddress);
             newEventElement.append(eventAddressElement);
             var eventDateElement = $("<p>").text("Date: " + eventDate);
             newEventElement.append(eventDateElement);
-            var weatherBtn = $("<button>").text("Show Weather").addClass("mb-2 btn btn-primary show-weather");
-            // detailsBtn.attr("data-address", eventAddress);
+            var weatherBtn = $("<button>").text("Show Weather").addClass("mb-2 mr-1 btn btn-primary show-weather");
             weatherBtn.attr("data-date", eventDate);
             weatherBtn.attr("data-event", eventCounter);
             newEventElement.append(weatherBtn);
+            var mapBtn = $("<button>").text("Show Venue on Map").addClass("mb-2 btn btn-primary show-map");
+            mapBtn.attr("data-address", eventAddress);
+            mapBtn.attr("data-venue", eventVenue)
+            newEventElement.append(mapBtn);
             newEventElement.addClass("border-bottom mt-2");
             $("#events-display").append(newEventElement);
             eventCounter++;
@@ -107,7 +112,8 @@ $("#clear-results").on("click", function (event){
   $("#search-keyword").val("");
   $("#to").val("");
   $("#from").val("");
-  $("map-card").hide();
+  callMap(eventAddress);
+  $("#map-card").hide();
 });
 
 // Attach click event to Show Details buttons 
@@ -146,3 +152,48 @@ $(document).on("click", ".show-weather", function(){
     }
   })
 });
+
+// Attach event handler to map button 
+$(document).on("click", ".show-map", function (){
+  eventAddress = $(this).attr("data-address");
+  locationTitle = $(this).attr("data-venue");
+  $("#map-title").text(locationTitle);
+  console.log(eventAddress);
+  callMap(eventAddress);
+})
+
+
+var eventAddress = '350 Fifth Avenue, New York, NY';
+var locationTitle = "New York City";
+
+// console.log(latlong);
+var callMap = function(eventAddress){ $.ajax({
+	url: 'https://maps.googleapis.com/maps/api/geocode/json?address=' +
+	eventAddress +
+  'CA&key=AIzaSyAW51bpjHefnDpcpjD-uvALl0jhTwaBFG8',
+	method: 'GET'
+}).then(function(data) {
+	console.log(data);
+	var eventLatitude = data.results[0].geometry.location.lat;
+	var eventLongitude = data.results[0].geometry.location.lng;
+	console.log(typeof eventLatitude);
+	console.log(typeof eventLongitude);
+
+	initMap(eventLatitude, eventLongitude, locationTitle);
+})
+};
+callMap(eventAddress);
+
+function initMap(eventLatitude, eventLongitude, locationTitle) {
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: { lat: eventLatitude, lng: eventLongitude },
+		zoom: 12
+	});
+
+	var markerPosition = { lat: eventLatitude, lng: eventLongitude };
+	var marker = new google.maps.Marker({
+		position: markerPosition,
+		map: map,
+		title: locationTitle
+	});
+}
